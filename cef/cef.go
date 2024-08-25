cd5ef97747990a15a0d860c8b9c8a7dcfd8332d7
杨红岩 <snxamdf@126.com>
2024-08-25 21:59:39 +0800

    v3: wv dev, local load

--- cef/cef.go
+++ cef/cef.go
@@ -167,8 +167,6 @@ func SetObjectInstance(object interface{}, instance unsafePointer) {
 	switch object.(type) {
 	case IObject:
 		lcl.SetObjectInstance(object.(IObject), instance)
-	case IUnknown:
-		lcl.SetUnknownInstance(object.(IUnknown), instance)
 	}
 }
 
@@ -180,8 +178,6 @@ func GetObjectUintptr(object interface{}) uintptr {
 	switch object.(type) {
 	case IObject:
 		return lcl.GetObjectUintptr(object.(IObject))
-	case IUnknown:
-		return lcl.GetUnknownUintptr(object.(IUnknown))
 	}
 	return 0
 }
